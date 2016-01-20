86ccfa481faadeb9f6f525f490fdeb921218fc64
jiehua <1753970025@qq.com>
2016-01-20 11:59:52 +0800

    add config.py

---
+++ config.sample.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+INFOBRIGHT = {
+    "host": "127.0.0.1:5029",
+    "user": "root",
+    "password": "root",
+    "database": "ipregion",
+    "local_infile": 1,
+}
+
+SERVER = {
+    "host": "127.0.0.1",
+    "port": 9999,
+}
+
+"""Gunicorn setting"""
+bind = "127.0.0.1:9999"
+workers = 1
+worker_class = "gevent"
+accesslog = "-"     # log to stderr
+errorlog = "-"      # log to stderr
+loglevel = "info"
